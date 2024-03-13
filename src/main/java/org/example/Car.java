cd9460702862613f9a92e9b70e8276844e271c30
basti <bastian_behrendt@yahoo.de>
2024-03-13 16:24:51 +0100

    Initial commit

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class Car {
+
+    String brand;
+    String model;
+    String color;
+    int YearOfManufacture;
+    int speed;
+    int newSpeed;
+
+
+    public Car(String brand, String model, String color, int YearOfManufacture, int speed, int newSpeed){
+
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.YearOfManufacture = YearOfManufacture;
+        this.speed = speed;
+        this.newSpeed = newSpeed;
+
+    }
+
+    public void startCar(){
+
+        System.out.println(
+                "The " + color + " " + brand + " " + model + " from " +  YearOfManufacture + " starts!" + " the speed is: " + speed
+        );
+
+    }
+
+    public void accelerateCar(int accelerate){
+
+        newSpeed = speed + accelerate;
+        System.out.println("The speed is now: " + newSpeed + " the old speed was: " + speed);
+
+    }
+
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Car("VW","Polo","Rot",1994, 200, 0);
+        car.startCar();
+        car.accelerateCar(50);
+
+    }
+
+}
